d70d8b1f6bd727036e6baab1960bc2e110e7d766
Ricardo Nava <ricardo1725@gmail.com>
2023-04-23 14:29:00 -0400

    array filter

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
 // Code your solution here
+
+function findMatching(drivers, name) {
+    return drivers.filter(driver => driver.toLowerCase() === name.toLowerCase());
+  }
+  
+  function fuzzyMatch(drivers, letters) {
+    return drivers.filter(driver => driver.startsWith(letters));
+  }
+  
+  function matchName(drivers, name) {
+    return drivers.filter(driver => driver.name === name);
+  }
+  
+  // Export functions
+  module.exports = {
+    findMatching,
+    fuzzyMatch,
+    matchName
+  };
